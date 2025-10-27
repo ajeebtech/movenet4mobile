92dc93ace6b39f5fc77d06073996a48d626a6aca
ajeebtech <jatintheboy@gmail.com>
2025-10-28 00:46:13 +0530

    working specimen

---
+++ MoveNetPoseTracker/MoveNetPoseTracker/MoveNetPoseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveNetPoseTrackerApp.swift
+//  MoveNetPoseTracker
+//
+//  Created by chubby  on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MoveNetPoseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoveNetPoseTracker/MoveNetPoseTrackerTests/MoveNetPoseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoveNetPoseTrackerTests.swift
+//  MoveNetPoseTrackerTests
+//
+//  Created by chubby  on 28/10/25.
+//
+
+import Testing
+@testable import MoveNetPoseTracker
+
+struct MoveNetPoseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
